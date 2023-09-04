8b2ea2fd0beeb74db4e89b690d070382d2ca4bd5
Romildo Silva Filho <romildo.silvafilho1@gmail.com>
2023-09-04 19:10:34 -0300

    feat: implementada a função search_by_word para casos de sucesso e falha, necessário refatorar para adotar um código limpo

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -22,4 +22,24 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    new_list = []
+
+    for archive in instance.get_data():
+        path_name = archive.get("nome_do_arquivo")
+        archive_lines = archive.get("linhas_do_arquivo")
+        ocorrencias = []
+
+        for number, line in enumerate(archive_lines, start=1):
+            if word.lower() in line.lower():
+                current = {
+                    "linha": number,
+                    "conteudo": line
+                }
+                ocorrencias.append(current)
+        if len(ocorrencias) > 0:
+            new_list.append({
+                "palavra": word,
+                "arquivo": path_name,
+                "ocorrencias": ocorrencias
+            })
+    return (new_list)
