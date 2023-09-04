758753ea0a93097737b0d0edf4845cc4b172aeea
Romildo Silva Filho <romildo.silvafilho1@gmail.com>
2023-09-03 23:08:03 -0300

    feat: implementado o método txt_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,17 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        if not path_file.endswith(".txt"):
+            raise ValueError("Formato inválido")
+
+        with open(path_file, "r") as file:
+            return file.read().split("\n")
+
+    except FileNotFoundError:
+        error_message = f"Arquivo {path_file} não encontrado"
+        print(error_message, file=sys.stderr)
+    except ValueError as e:
+        error_message = str(e)
+        print(error_message, file=sys.stderr)
